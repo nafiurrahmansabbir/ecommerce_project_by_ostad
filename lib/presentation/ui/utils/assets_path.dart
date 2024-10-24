9dd94a89bbfdb03b7fbfb1daf702db44e59ee7aa
nafiurrahmansabbir <nafiurrahmansabbir@gmail.com>
2024-10-24 23:20:20 +0600

    all done

--- lib/presentation/ui/utils/assets_path.dart
+++ lib/presentation/ui/utils/assets_path.dart
@@ -4,4 +4,6 @@ class AssetsPath {
   static const String appLogo = '$_images/logo.svg';
   static const String appLogoNav = '$_images/logo_nav.svg';
   static const String dummyProductImg = '$_images/shoe.png';
+
+
 }
